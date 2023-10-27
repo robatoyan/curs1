91a6f1e886ad296faa1dba2295070000b841b94c
robatoyan <142260538+robatoyan@users.noreply.github.com>
2023-10-27 13:07:12 +0400

    add new file

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Show from "../views/Show.vue"
+import AddUser from "../views/AddUser.vue"
+import Apply from "../views/Apply.vue"
+import SeeMore from "../views/SeeMore.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'show',
+      component: Show
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/Add',
+      name: 'addUser',
+      component: AddUser
+    },
+    {
+      path: '/Apply',
+      name: 'apply',
+      component: Apply
+    },
+    {
+      path: '/See/:id',
+      name: 'seeMore',
+      component: SeeMore
     }
   ]
 })
---
+++ src/views/Show.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <h2>Show</h2>
+</template>
+
+<style></style>
